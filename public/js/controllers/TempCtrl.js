9e467bd8030f021816311ff664c4aa43c68cd5a5
leandro2m <lmmoraes@gmail.com>
2017-03-07 08:09:54 -0300

    temperatura ctrl

--- public/js/controllers/TempCtrl.js
+++ public/js/controllers/TempCtrl.js
@@ -1,12 +1,25 @@
-angular.module('App').controller("TempCtrl", function($scope, $http) {
-	$scope.app = "App";
-	$scope.items = [];
+angular.module('App').controller('TempCtrl', function($scope,$resource){
+   
+     $scope.exportData = function () {
+        var blob = new Blob([document.getElementById('exportable').innerHTML], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+        });
+        saveAs(blob, "Temp.xls");
+    };
 
-	var carregaDados = function() {
-		$http.get("https://leandro2m.herokuapp.com/api/data1").then(function(response) {
-			$scope.items = response.data
-			
-		});
+	$scope.items = [];
+	
+	var apiData = $resource('/api/data1/:id');
+	function buscaData() {
+		apiData.query(
+			function(items) {		
+				$scope.items = items;
+			},
+			function(erro) {
+				console.log(erro);
+				$scope.mensagem = {texto: 'Não foi possível obter os dados'};
+			}
+		);
 	}
-  carregaDados();
+buscaData();
 })

07834abf3f3d49ac406cbd5547e5147669a1171d
leandro2m <lmmoraes@gmail.com>
2017-03-08 15:17:00 -0300

    XHR right

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-f
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-app.use(function(req, res, next){
-	res.setHeader('Access-Control-Allow-Origin', '*');
-})
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
 
